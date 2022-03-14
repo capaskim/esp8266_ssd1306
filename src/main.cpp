005a40ec32ff25a2e519820f52b00da9bff780df
Capas Kim <capas.kim@gmail.com>
2022-03-15 00:33:06 +0900

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,45 @@
 #include <Arduino.h>
 #include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h"
+#include "dialog_14.h"
 
 #define SDA 10
 #define SCL 9
 
 SSD1306Wire display(0x3c, SDA, SCL);
+char buffer[50];
+int i;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   display.init();
   display.flipScreenVertically();
+  display.setFont(Dialog_plain_14);
+  i = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int i = 0;
   while(i < 256) {
     display.clear();
-    analogWrite(D4, i);
-    Serial.printf("Hello %d입니다.\n", i);
-    display.drawString(0, 0, String(i));
+    analogWrite(D4, 255-i);
+    Serial.printf("Hello %.1f %%입니다.\n", i/255.0*100);
+    display.drawStringf(0, 0, buffer, "Bright: %.1f  %%", i/255.0*100);
     display.display();
-    delay(5);
+    delay(100);
     i++;
   }
-  delay(3000);
+  delay(1000);
 
   while(i >= 0) {
-    analogWrite(D4, i);
-    Serial.printf("Hi %d입니다.\n", i);
+    analogWrite(D4, 255-i);
+    Serial.printf("Hi %.1f %%입니다.\n", float(i)/255*100);
     display.clear();
-    display.drawString(0, 0, String(i));
+    display.drawStringf(0, 0, buffer, "Bright: %.1f %%", i/255.0*100);
     display.display();   
-    delay(5);
+    delay(100);
     i--;
   }
-  delay(1000);
+  delay(3000);
 }
